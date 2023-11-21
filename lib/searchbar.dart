d2518ed193ffe4a2593fca5ec762736f25c7c591
Raphael2b3 <raphael.schuetz0311@gmail.com>
2023-11-21 20:39:22 +0100

    dvdfvd

--- lib/searchbar.dart
+++ lib/searchbar.dart
@@ -10,42 +10,41 @@ class Searchbar extends StatefulWidget {
 class _SearchbarState extends State<Searchbar> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.centerRight,
-      children: [
-        Container(
-          height: 67,
-          decoration: const BoxDecoration(
-            color: Color.fromRGBO(37, 37, 37, 1),
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10),
-                bottomLeft: Radius.circular(10),
-                topRight: Radius.circular(67),
-                bottomRight: Radius.circular(67)),
-          ),
-          child: const TextField(
-            style: TextStyle(fontSize: 18, color: Colors.white),
-            maxLines: null,
-            minLines: null,
-            expands: true,
-            textAlignVertical: TextAlignVertical.center,
-            decoration: InputDecoration(
-              hintStyle: TextStyle(color: Colors.grey),
-              hintText: "Search...",
-              contentPadding: EdgeInsets.only(left: 13, right: 10),
-              border: InputBorder.none,
+    return Container(
+      height: 70,
+      decoration: const BoxDecoration(
+          color: Color.fromRGBO(37, 37, 37, 1),
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: Row(
+        children: [
+          const Expanded(
+            child: TextField(
+              style: TextStyle(fontSize: 18, color: Colors.white),
+              maxLines: null,
+              minLines: null,
+              expands: true,
+              textAlignVertical: TextAlignVertical.center,
+              decoration: InputDecoration(
+                hintStyle: TextStyle(color: Colors.grey),
+                hintText: "Search...",
+                contentPadding: EdgeInsets.only(left: 13, right: 10),
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              cursorColor: Color(0xFFFFFFFF),
             ),
-            cursorColor: Color(0xFFFFFFFF),
           ),
-        ),
-        Container(
-          height: 80,
-          width: 80,
-          decoration: const BoxDecoration(
-              color: Colors.blueAccent,
-              borderRadius: BorderRadius.all(Radius.circular(70))),
-        )
-      ],
+          IconButton(onPressed: () => {}, icon: const Icon(Icons.clear)),
+          Container(
+            height: 80,
+            width: 80,
+            decoration: const BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.all(Radius.circular(70))),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/statement.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Statement extends StatefulWidget {
+  const Statement({super.key});
+
+  @override
+  State<Statement> createState() => _StatementState();
+}
+
+class _StatementState extends State<Statement> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
